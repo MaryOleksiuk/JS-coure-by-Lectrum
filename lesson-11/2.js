f1bdb2510410ab8024edb47552b0d23b671be8de
MaryOleks <mariia.oleksiuk@omnigon.com>
2019-07-28 10:26:17 +0300

    Add homework for lesson 11

---
+++ lesson-11/2.js
@@ -0,0 +1,38 @@
+/**
+ * Задача 2.
+ *
+ * Создайте функцию createFibonacciGenerator(),
+ * которая вернёт ещё одну функцию,
+ * каждый вызов которой будет возвращать число из последовательности Фибоначчи.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+function createFibonacciGenerator() {
+  let rootNumber = 0;
+  let nextNumber = 1;
+
+  return function() {
+    let oldRootNumber = rootNumber;
+    let oldNextNumber = nextNumber;
+
+    rootNumber = nextNumber;
+    nextNumber = oldRootNumber + oldNextNumber;
+
+    return rootNumber;
+  }
+}
+
+const generateFibonacciNumber = createFibonacciGenerator();
+
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 2
+console.log(generateFibonacciNumber()); // 3
+console.log(generateFibonacciNumber()); // 5
+console.log(generateFibonacciNumber()); // 8
+console.log(generateFibonacciNumber()); // 13
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
---
+++ lesson-11/3.js
@@ -0,0 +1,53 @@
+/**
+ * Задача 3.
+ *
+ * Улучшите функцию createFibonacciGenerator() из предыдущего примера.
+ *
+ * Теперь вызов функции createFibonacciGenerator() должен возвращать объект, который содержит два метода:
+ * - print — возвращает число из последовательности Фибоначчи;
+ * - reset — обнуляет последовательность и ничего не возвращает.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+function createFibonacciGenerator() {
+  let rootNumber = 0;
+  let nextNumber = 1;
+
+  return {
+    print: function() {
+      let oldRootNumber = rootNumber;
+      let oldNextNumber = nextNumber;
+
+      rootNumber = nextNumber;
+      nextNumber = oldRootNumber + oldNextNumber;
+
+      return rootNumber;
+    },
+    reset: function() {
+      rootNumber = 0;
+      nextNumber = 1;
+    }
+  };
+}
+
+const generator1 = createFibonacciGenerator();
+
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+console.log(generator1.print()); // 3
+console.log(generator1.reset()); // undefined
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 1
+console.log(generator1.print()); // 2
+
+const generator2 = createFibonacciGenerator();
+
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 1
+console.log(generator2.print()); // 2
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
